3c423059d8c4fac685cf5fc818db85c88266d16b
gtsaito <gtsaito@mppr.mp.br>
2023-08-29 14:01:00 -0300

    Provavel termino do algoritmo genetico

--- algoritmo_genetico/main.py
+++ algoritmo_genetico/main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 def get_data_ids():
     list_ids = []
@@ -27,186 +28,105 @@ def get_data_pesos():
 
     return list_pesos
 
-# ids = get_data_ids()
-# valores = get_data_valores()
-# pesos = get_data_pesos()
 
-# print(ids)
-# print(valores)
-# print(pesos)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import random
-import sys
-import operator
-
-class Knapsack(object):	
-
-	#initialize variables and lists
-	def __init__(self):	
-
-		self.C = 0
-		self.weights = []
-		self.profits = []
-		self.opt = []
-		self.parents = []
-		self.newparents = []
-		self.bests = []
-		self.best_p = [] 
-		self.iterated = 1
-		self.population = 0
-
-		# increase max recursion for long stack
-		iMaxStackSize = 1500000
-		sys.setrecursionlimit(iMaxStackSize)
-
-	# create the initial population 
-	def initialize(self):
-
-		for i in range(self.population):
-			parent = []
-			for k in range(0, 5):
-				k = random.randint(0, 1)
-				parent.append(k)
-			self.parents.append(parent)
-
-	# set the details of this problem
-	def properties(self, weights, profits, opt, C, population):
-
-		self.weights = weights
-		self.profits = profits
-		self.opt = opt
-		self.C = C
-		self.population = population
-		self.initialize()
-
-	# calculate the fitness function of each list (sack)
-	def fitness(self, item):
-
-		sum_w = 0
-		sum_p = 0
-
-		# get weights and profits
-		for index, i in enumerate(item):
-			if i == 0:
-				continue
-			else:
-				sum_w += self.weights[index]
-				sum_p += self.profits[index]
-
-		# if greater than the optimal return -1 or the number otherwise
-		if sum_w > self.C:
-			return -1
-		else: 
-			return sum_p
-	
-	# run generations of GA
-	def evaluation(self):
-
-		# loop through parents and calculate fitness
-		best_pop = self.population // 2
-		for i in range(len(self.parents)):
-			parent = self.parents[i]
-			ft = self.fitness(parent)
-			self.bests.append((ft, parent))
-
-		# sort the fitness list by fitness		
-		self.bests.sort(key=operator.itemgetter(0), reverse=True)
-		self.best_p = self.bests[:best_pop]
-		self.best_p = [x[1] for x in self.best_p]
-
-	# mutate children after certain condition
-	def mutation(self, ch):
-
-		for i in range(len(ch)):		
-			k = random.uniform(0, 1)
-			if k > 0.5:
-				#if random float number greater that 0.5 flip 0 with 1 and vice versa
-				if ch[i] == 1:
-					ch[i] = 0
-				else: 
-					ch[i] = 1
-		return ch
-
-	# crossover two parents to produce two children by miixing them under random ration each time
-	def crossover(self, ch1, ch2):
-
-		threshold = random.randint(1, len(ch1)-1)
-		tmp1 = ch1[threshold:]
-		tmp2 = ch2[threshold:]
-		ch1 = ch1[:threshold]
-		ch2 = ch2[:threshold]
-		ch1.extend(tmp2)
-		ch2.extend(tmp1)
-
-		return ch1, ch2
-
-	# run the GA algorithm
-	def run(self):
-
-		# run the evaluation once
-		self.evaluation()
-		newparents = []
-		pop = len(self.best_p)-1
-
-		# create a list with unique random integers
-		sample = random.sample(range(pop), pop)
-		for i in range(0, pop):
-			# select the random index of best children to randomize the process
-			if i < pop-1:
-				r1 = self.best_p[i]
-				r2 = self.best_p[i+1]
-				nchild1, nchild2 = self.crossover(r1, r2)
-				newparents.append(nchild1)
-				newparents.append(nchild2)
-			else:
-				r1 = self.best_p[i]
-				r2 = self.best_p[0]
-				nchild1, nchild2 = self.crossover(r1, r2)
-				newparents.append(nchild1)
-				newparents.append(nchild2)
-
-		# mutate the new children and potential parents to ensure global optima found
-		for i in range(len(newparents)):
-			newparents[i] = self.mutation(newparents[i])
-
-		if self.opt in newparents:
-			print ("optimal found in {} generations" .format(self.iterated))
-		else:
-			self.iterated += 1
-			print("recreate generations for {} time" .format(self.iterated))
-			self.parents = newparents
-			self.bests = []
-			self.best_p = []
-			self.run()	
-
-# properties for this particular problem
-# weights = [12,  7, 11, 8, 9]
 weights = get_data_pesos()
-# profits = [24, 13, 23, 15, 16]
-profits = get_data_valores()
-opt     = [1, 1, 1, 0, 0, 0, 0, 1, 1, 0]
-C = 26
-population = 10
+values = get_data_valores()
+knapsack_capacity = 80
+num_items = len(weights)
+population_size = 50
+num_generations = 100
+mutation_probability = 0.2
 
-k = Knapsack()
-k.properties(weights, profits, opt, C, population)
-k.run()
+# Inicializa a populacao determinando quais objetos vao entrar na mochila (1) ou n vao (0)
+def initialize_population(size):
+	list_population = []
+	for _ in range(size):
+		population = []
+		for _ in range(num_items):
+			population.append(random.randint(0, 1))
+		list_population.append(population)
+
+	return list_population
+
+# Verifica o valor de cada individuo (cada vetorzinho do vetorzao populacao)
+def evaluate_individual(individual):
+	total_weight = 0
+	for i in range(num_items):
+		# Vai contar o peso dos itens somente os itens que estao dentro da mochila (1)
+		if individual[i]:
+			total_weight += weights[i]
+
+	# Se o peso total dos itens ultrapassar a capacidade da mochila, entao tem que ser penalizado
+	if total_weight > knapsack_capacity:
+		return 0  # Penalize solutions that exceed capacity
+	
+	total_value = 0
+	for i in range(num_items):
+		# Verifica o valor total dos itens que estao dentro da mochila e estao respeitado o valor total
+		if individual[i]:
+			total_value += values[i]
+
+	return total_value
+
+# Gera a crianca a paritr de 2 pais
+def crossover(parent1, parent2):
+	# Index para pegar os genes dos pais
+	crossover_point = random.randint(1, int((num_items - 1) / 2))
+
+	child = parent1[:crossover_point] + parent2[crossover_point:]
+	
+	return child
+
+# Altera os genes da crianca (muta) caso a probabilidade seja maior que o numero randomico float de 0 a 1, serve para nao ficar parado em um maximo local
+def mutate(individual):
+    for i in range(num_items):
+        if random.random() < mutation_probability:
+			# Caso um numero randomico float entre 1 e 0 for menor que a probabilidade de mutacao, ele vai flipar o binario, 1 vira 0 e 0 vira 1
+            individual[i] = 1 - individual[i]
+
+def genetic_algorithm():
+	# Gera a populacao inicial
+	population = initialize_population(population_size)
+
+	# Vai rodar x geracoes especificadas por mim, caso encontre a melhor solucao, pode parar
+	for generation in range(num_generations):
+		fitness_values = []
+		for ind in population:
+			# Verifica o valor total dos itens de cada individuo da populacao
+			fitness_values.append(evaluate_individual(ind))
+
+		# Pega o maior valor dos itens de todos os individuos da populacao
+		best_fitness = max(fitness_values)
+		print(f"Generation {generation+1}: Best Fitness = {best_fitness}")
+
+		# Caso tenha encontrado algum individuo que possua o maior valor possivel (maximo global), pode parar
+		if best_fitness == 382:
+			break
+
+		# Olhando todas as population_size, escolher uma quantidade population_size dos vetores, sendo que a probabilidade de ser escolhida eh pelo fitness_values
+		parents = random.choices(population, weights = fitness_values, k = population_size)
+
+		# Para cada dupla de parentes, cria 2 criancas pegando parte de genes de cada 1, sendo esses genes aleatorios
+		offspring = []
+		for i in range(0, population_size, 2):
+			parent1, parent2 = parents[i], parents[i + 1]
+			child1 = crossover(parent1, parent2)
+			child2 = crossover(parent2, parent1)
+			mutate(child1)
+			mutate(child2)
+			offspring.extend([child1, child2])
+
+		# Atualiza a populacao para as novas criancas
+		population = offspring
+	
+	# Acha o index em que tem o maior valor dos itens dos individuos na populacao
+	best_fitness_index = fitness_values.index(max(fitness_values))
+	# Com o index achado, eh possivel ver qual individuo produzio tal resultado e mostra ele
+	best_individual = population[best_fitness_index]
+
+	print("Best Solution:")
+	print("Chromosome:", best_individual)
+	print("Total Value:", evaluate_individual(best_individual))
+
+# Run the genetic algorithm
+genetic_algorithm()
