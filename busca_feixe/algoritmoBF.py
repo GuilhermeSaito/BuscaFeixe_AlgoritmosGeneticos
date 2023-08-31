38030a0e4b5e1973d27b41827b77ba53be6532bf
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-08-31 19:47:44 -0300

    melhorando o código.

--- busca_feixe/algoritmoBF.py
+++ busca_feixe/algoritmoBF.py
@@ -36,21 +36,32 @@ def initialize_solutions(beam_width):
 
 # Função de avaliação usando tempera simulada
 def simulated_annealing_evaluation(solution):
+    # Pega o valor total de todos os pesos
+    total_all_weight = 0
+    for weight in weights:
+        total_all_weight += weight
+
     total_weight = 0
     for i in range(num_items):
         if solution[i]:
             total_weight += weights[i]
 
-        # Se o peso total dos itens ultrapassar a capacidade da mochila, terá sua solução penalizada
-        if total_weight > knapsack_capacity:
-            return 0 
-        
-        total_value = 0
-        for i in range(num_items):
-            # Verifica o valor total dos itens que estao dentro da mochila e estao respeitado o valor total
-            if solution[i]:
-                total_value += values[i]
-        return total_value
+    # Calculando a penalidade de acordo com esse paper http://repository.lppm.unila.ac.id/1079/1/jeas_0416_4013-2.pdf
+    dist = abs(total_weight - knapsack_capacity)
+    diff = min(knapsack_capacity, abs(total_all_weight - knapsack_capacity))
+    penalty = 1 - (dist / diff)
+    if penalty <= 0:
+        penalty = 0.00001
+
+	# Se o peso total dos itens ultrapassar a capacidade da mochila, entao tem que ser penalizado
+    if total_weight > knapsack_capacity:
+        return total_weight * penalty
+    total_value = 0
+    for i in range(num_items):
+        # Verifica o valor total dos itens que estao dentro da mochila e estao respeitado o valor total
+        if solution[i]:
+            total_value += values[i]
+    return total_value
 
 #Gera os vizinhos obtidos através de pertubação da solução atual
 def generate_neighbor(solution):
@@ -62,19 +73,29 @@ def generate_neighbor(solution):
 def beam_width_algorithm():
     #Gera o conjunto de soluções iniciais
     list_initial_solution = initialize_solutions(beam_width)
+    best_fit = 0
+    best_solution = 0
+    best_iteration = 0 
     
     # Roda x vezes, parando ao encontrar a melhor solução   
-    for _ in range(num_interations):
+    for a in range(num_iterations):
         next_solutions = []
         temperature = 10
         # Gerar vizinhos e avaliar soluções atuais
         for solution in list_initial_solution:
-            
             neighbors = [generate_neighbor(solution) for _ in range(num_neighbors)] # Gera num_neighbors vizinhos perturbando a solução atual
             neighbors_with_scores = [(neighbor, simulated_annealing_evaluation(neighbor)) for neighbor in neighbors] # Aplicando a função avaliativa para cada solução
             neighbors_with_scores.sort(key=lambda x: x[1], reverse=True) # Classificando os vizinhos pela avaliação em ordem decrescente
-            
-            next_solutions.extend([neighbor for neighbor, _ in neighbors_with_scores[:beam_width]]) # Seleciona os beam_width melhores vizinhos para a próxima iteração
+       
+        max_value = max(neighbors_with_scores)[-1]
+        best_neighbor = max(neighbors_with_scores)[0]
+        
+        next_solutions.extend([neighbor for neighbor, _ in neighbors_with_scores[:beam_width]]) # Seleciona os beam_width melhores vizinhos para a próxima iteração
+        
+        if max_value > best_fit:
+           best_fit = max_value
+           best_iteration = a
+           best_solution = best_neighbor            
             
         # Processo de Tempera Simulada para aceitar/rejeitar soluções perturbadas
         for i in range(len(next_solutions)):
@@ -82,18 +103,24 @@ def beam_width_algorithm():
             perturbed_solution = generate_neighbor(next_solutions[i])
 
             new_score = simulated_annealing_evaluation(perturbed_solution)
+            
             if new_score > old_score or random.random() < math.exp((new_score - old_score) / temperature):
                 next_solutions[i] = perturbed_solution
-            
+
         # Atualiza as soluções atuais para as próximas soluções geradas
         current_solutions = next_solutions
 
         # Diminui a temperatura para controlar a probabilidade de aceitar soluções piores
         temperature *= 0.95  # Fator de resfriamento
         
+    print("Best fit", best_fit)
+    print("Best iteracao", best_iteration)
+    print("Best solucao", best_solution)
+    
     # Encontra a melhor solução entre as soluções atuais
     best_solution = max(current_solutions, key=simulated_annealing_evaluation)
-    return best_solution      
+    return best_solution    
+
 
 # Definir uma heurística para estes parâmetros
 weights = get_data_pesos()
@@ -103,7 +130,7 @@ num_items = len(weights)
 beam_width = 5
 num_neighbors = 5
 temperature = 10
-num_interations = 10
+num_iterations = 100
     
 best_solution = beam_width_algorithm()
 
